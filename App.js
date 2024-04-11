ef009d96bb1a6567b37375e22c3dbc6532750726
Mittal-Malankiya <m4mittal19@gmail.com>
2024-04-11 14:31:03 +0200

    update app.js

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// import the screens
+import ShoppingLists from "./components/ShoppingLists";
+import Welcome from "./components/Welcome";
+
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyB_Xi0mlWQTcKJMOyiakfkT-UTGDo0teSg",
+    authDomain: "shopping-list-demo-2f425.firebaseapp.com",
+    projectId: "shopping-list-demo-2f425",
+    storageBucket: "shopping-list-demo-2f425.appspot.com",
+    messagingSenderId: "348294468851",
+    appId: "1:348294468851:web:e27ab3e457971fb3855386",
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="ShoppingLists">
+          {(props) => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
